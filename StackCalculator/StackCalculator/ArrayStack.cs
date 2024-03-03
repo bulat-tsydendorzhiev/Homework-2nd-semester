df2776eb5397a6bd426e3463456806b9e91a7ac6
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2024-03-03 23:29:37 +0300

    add task "Stack Calculator" with tests.

---
+++ StackCalculator/StackCalculator/ArrayStack.cs
@@ -0,0 +1,57 @@
+﻿namespace StackCalculator;
+
+/// <summary>
+/// Implementation of a stack on an array.
+/// </summary>
+public class ArrayStack : IStack
+{
+    private const int InitialCapacity = 10;
+    private double[] StackElements;
+    private int Count;
+
+    /// <summary>
+    /// Initializes a new object ArrayStack.
+    /// </summary>
+    public ArrayStack()
+    {
+        StackElements = new double[InitialCapacity];
+    }
+
+    /// <inheritdoc />
+    public bool IsEmpty() => Count == 0;
+
+    /// <inheritdoc />
+    public void Push(double value)
+    {
+        if (Count == StackElements.Length)
+        {
+            Array.Resize(ref StackElements, StackElements.Length * 2);
+        }
+
+        StackElements[Count++] = value;
+    }
+
+    /// <inheritdoc />
+    public double Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is empty. You can't delete anything from it.");
+        }
+
+        --Count;
+        var value = StackElements[Count];
+        StackElements[Count] = 0;
+
+        return value;
+    }
+
+    /// <inheritdoc />
+    public void Clear()
+    {
+        StackElements = new double[InitialCapacity];
+        Count = 0;
+    }
+
+}
+
---
+++ StackCalculator/StackCalculator/ListStack.cs
@@ -0,0 +1,46 @@
+﻿namespace StackCalculator;
+
+/// <summary>
+/// Implementation of a stack on an list.
+/// </summary>
+public class ListStack : IStack
+{
+    private readonly List<double> StackElements;
+
+    /// <summary>
+    /// Initializes a new object ListStack.
+    /// </summary>
+    public ListStack()
+    {
+        StackElements = new List<double>();
+    }
+
+    /// <inheritdoc />
+    public bool IsEmpty() => StackElements.Count == 0;
+
+    /// <inheritdoc />
+    public void Push(double value)
+    {
+        StackElements.Add(value);
+    }
+
+    /// <inheritdoc />
+    public double Pop()
+    {
+        if (StackElements.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty. You can't delete anything from it.");
+        }
+
+        var value = StackElements.Last();
+        StackElements.RemoveAt(StackElements.Count - 1);
+        return value;
+    }
+
+    /// <inheritdoc />
+    public void Clear()
+    {
+        StackElements.Clear();
+    }
+
+}
