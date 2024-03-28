44512470a61c3c13629dab3779e333b361957947
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2024-03-28 04:17:50 +0300

    Add previous files, tests; made minor changes

--- ParseTree/ParseTree/Program.cs
+++ ParseTree/ParseTree/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using ParseTree;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Incorrect file path. Use -help to learn more.");
+    return;
+}
+
+if (args[0] == "-help")
+{
+    Console.WriteLine("Enter path to file with expression written in prefix form.");
+    return;
+}
+
+string filePath = args[0];
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("File with such path doesn't exist.");
+    return;
+}
+
+var expression = File.ReadAllText(filePath);
+
+ParseTree.ParseTree tree = new();
+
+try 
+{
+    tree.Build(expression);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+try
+{
+    double result = tree.Calculate();
+    Console.WriteLine("Calculation result");
+    tree.Print();
+    Console.WriteLine($"={result}");
+}
+catch (ArgumentException ex1)
+{
+    Console.WriteLine(ex1.Message);
+}
+catch(InvalidOperationException ex2)
+{
+    Console.WriteLine(ex2.Message);
+}
---
+++ ParseTree/ParseTreeTests/ParseTreeTests.cs
@@ -0,0 +1,86 @@
+using ParseTree;
+
+namespace ParseTreeTests;
+
+public class Tests
+{
+    private const float Epsilon = 1e-7f;
+    
+    [Test]
+    public void BuildingNullExpressionShouldThrowArgumentNullException()
+    => Assert.Throws<ArgumentNullException>(() => new ParseTree.ParseTree().Build(null));
+    
+    [Test]
+    public void BuildingEmptyExpressionShouldThrowArgumentException()
+        => Assert.Throws<ArgumentException>(() => new ParseTree.ParseTree().Build(""));
+    
+    [TestCaseSource(nameof(InvalidExpressions))]
+    public void BuildingEmptyStringShouldThrowArgumentException(string expression)
+        => Assert.Throws<ArgumentException>(() => new ParseTree.ParseTree().Build(expression));
+    
+    [Test]
+    public void CalculatingNullExpressionShouldThrowInvalidOperationException()
+        => Assert.Throws<InvalidOperationException>(() => new ParseTree.ParseTree().Calculate());
+    
+    [TestCaseSource(nameof(ExpressionsWithDivizionByZero))]
+    public void CalculatingExpressionWithDivizionByZeroShouldThrowArgumentException(string expression)
+    {
+        ParseTree.ParseTree tree = new();
+        tree.Build(expression);
+        Assert.Throws<ArgumentException>(() => tree.Calculate());
+    }
+    
+    [TestCaseSource(nameof(ValidExpressionsAndThierCorrectCalculatedResults))]
+    public void CalculatingValidExpressionShouldReturnRightAnswer(string expression, double rightAnswer)
+    {
+        ParseTree.ParseTree tree = new();
+        tree.Build(expression);
+        Assert.That(Math.Abs(tree.Calculate() - rightAnswer), Is.LessThan(Epsilon));
+    }
+    
+    private static IEnumerable<TestCaseData> ValidExpressionsAndThierCorrectCalculatedResults
+    {
+        get
+        {
+            yield return new TestCaseData("(+ 10 18)", 28);
+            yield return new TestCaseData("(+ 1 0)", 1);
+            yield return new TestCaseData("(- 1 0)", 1);
+            yield return new TestCaseData("(+ 0 0)", 0);
+            yield return new TestCaseData("(* 0 0)", 0);
+            yield return new TestCaseData("(- 10 18)", -8);
+            yield return new TestCaseData("(- -10 10)", -20);
+            yield return new TestCaseData("(* (+ 1 1) 2) ", 4);
+            yield return new TestCaseData("(/ 16 2)", 8);
+            yield return new TestCaseData("(/ 5 2)", 2.5);
+            yield return new TestCaseData("(* 5 0)", 0);
+            yield return new TestCaseData("(* -5 0)", 0);
+        }
+    }
+    
+    private static IEnumerable<TestCaseData> InvalidExpressions
+    {
+        get
+        {
+            yield return new TestCaseData("Just a text");
+            yield return new TestCaseData("()");
+            yield return new TestCaseData("(+ 1 1))");
+            yield return new TestCaseData("((+ 1 1)");
+            yield return new TestCaseData("(- -10 10 10)");
+            yield return new TestCaseData("(* (% 5 1) 2) ");
+            yield return new TestCaseData("(- - -)");
+            yield return new TestCaseData("5 5 5 5 5 5 5");
+            yield return new TestCaseData("(* 12 (+ 7))");
+        }
+    }
+    
+    private static IEnumerable<TestCaseData> ExpressionsWithDivizionByZero
+    {
+        get
+        {
+            yield return new TestCaseData("(/ 2 0)");
+            yield return new TestCaseData("(/ -2 0)");
+            yield return new TestCaseData("(/ (+ 1 1) (- 1 1))");
+        }
+    }
+}
+
