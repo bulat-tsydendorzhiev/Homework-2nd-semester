b140b86720bb087a28f4c740833e6aaf7ad95699
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2024-04-28 20:13:15 +0300

    Add main functional and some tests without enumerator functional

--- SkipList/SkipList/SkipList.cs
+++ SkipList/SkipList/SkipList.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 
-namespace Lists;
+namespace SkipListLibrary;
 
 /// <summary>
 /// Skip list, a probabilistic data structure that provides on average the logarithmic complexity of basic operations.
@@ -9,97 +9,250 @@ namespace Lists;
 public class SkipList<T> : IList<T>
     where T: IComparable<T>
 {
+    private const int MaxLevel = 5;
+
+    private int _currentMaxLevel = 1; 
+
     private readonly Random _random = new();
 
-    private SkipListElement _head;
+    private SkipListNode _head;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="SkipList"/>.
+    /// </summary>
+    public SkipList()
+    {
+        _head = new(default, _currentMaxLevel);
+    }
 
     /// <inheritdoc/>
     public T this[int index]
     {
         get
         {
-            return default;
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            var currentNode = _head;
+
+            for (int i = 0; i < index; ++i)
+            {
+                currentNode = currentNode.Next[0];
+            }
+
+            return currentNode.Value;
         }
-        set => throw new NotSupportedException("Skip list doesn't support setting value.");
+        set => throw new NotSupportedException("Skip list doesn't support set value operation.");
     }
 
     /// <inheritdoc/>
-    public int Count {get; }
+    public int Count { get; private set; }
 
     /// <inheritdoc/>
-    public bool IsReadOnly => false;
+    bool ICollection<T>.IsReadOnly => false;
 
     /// <inheritdoc/>
-    public void Add(T item)
+    public void Add(T value)
     {
+        var newNodeLevelsAmount = GetLevelsAmount();
+        var newNode = new SkipListNode(value, newNodeLevelsAmount);
+
+        var currentNode = _head;
+
+        for (int i = _currentMaxLevel - 1; i >= 0; --i)
+        {
+            while (currentNode.Next[i] is not null)
+            {
+                if (value.CompareTo(currentNode.Value) < 0)
+                {
+                    break;
+                }
+
+                currentNode = currentNode.Next[i];
+            }
+
+            if (i < newNodeLevelsAmount)
+            {
+                newNode.Next[i] = currentNode.Next[i];
+                currentNode.Next[i] = newNode;
+            }
+        }
         
+        ++Count;
     }
 
     /// <inheritdoc/>
     public void Clear()
     {
-        
+        Count = 0;
+        _currentMaxLevel = 1;
+        _head = new(default, _currentMaxLevel);
     }
 
     /// <inheritdoc/>
-    public bool Contains(T item)
+    public bool Contains(T value)
     {
-        return true;
+        return IndexOf(value) != -1;
     }
 
     /// <inheritdoc/>
     public void CopyTo(T[] array, int arrayIndex)
     {
-        
+        ArgumentNullException.ThrowIfNull(array);
+
+        if (arrayIndex < 0 || arrayIndex >= Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+        }
+
+        if (arrayIndex >= Count)
+        {
+            throw new ArgumentException("Index must be less than amount of elements in skip list.");
+        }
+
+        var currentNode = _head;
+
+        for (int i = 0; i < Count; ++i)
+        {
+            currentNode = currentNode.Next[0];
+
+            if (i >= arrayIndex)
+            {
+                array[i] = currentNode.Value;
+            }
+        }
     }
 
     /// <inheritdoc/>
-    public int IndexOf(T item)
+    public int IndexOf(T value)
     {
-        return 1;
+        var currentNode = _head;
+        int index = 0;
+        
+        for (int i = _currentMaxLevel - 1; i >= 0; --i)
+        {
+            while (currentNode.Next[i] is not null)
+            {
+                if (value.CompareTo(currentNode.Value) < 0)
+                {
+                    break;
+                }
+                else if (value.CompareTo(currentNode.Value) == 0)
+                {
+                    return index;
+                }
+
+                currentNode = currentNode.Next[i];
+                ++index;
+            }
+        }
+        
+        return -1;
     }
 
-    /// <inheritdoc/>
-    public void Insert(int index, T item)
+    /// <summary>
+    /// Not supported operation.
+    /// </summary>
+    public void Insert(int index, T value)
         => throw new NotSupportedException("Skip list doesn't support insert operation.");
 
     /// <inheritdoc/>
-    public bool Remove(T item)
+    public bool Remove(T value)
     {
-        return true;
+        var successfulRemoving = false;
+        var currentNode = _head;
+        
+        for (int i = _currentMaxLevel - 1; i >= 0; --i)
+        {
+            while (currentNode.Next[i] is not null)
+            {
+                if (value.CompareTo(currentNode.Value) < 0)
+                {
+                    break;
+                }
+                else if (value.CompareTo(currentNode.Next[i].Value) == 0)
+                {
+                    currentNode.Next[i] = currentNode.Next[i].Next[i];
+                    successfulRemoving = true;
+                }
+
+                currentNode = currentNode.Next[i];
+            }
+        }
+        
+        if (successfulRemoving)
+        {
+            --Count;
+        }
+        
+        return successfulRemoving;
     }
 
     /// <inheritdoc/>
     public void RemoveAt(int index)
     {
+        Remove(this[index]);
+    }
+
+    private int GetLevelsAmount()
+    {
+        int levelsAmount = 1;
         
+        while (levelsAmount < MaxLevel && CanContinue())
+        {
+            ++levelsAmount;
+        }
+        
+        return levelsAmount;
+    }
+
+    private bool CanContinue()
+    {
+        return _random.Next(0, 2) == 1;
     }
 
     /// <inheritdoc/>
     public Enumerator GetEnumerator()
     {
-        return new Enumerator();
+        return new(this);
     }
 
     IEnumerator<T> IEnumerable<T>.GetEnumerator()
     {
-        throw new NotImplementedException();
+        return new Enumerator(this);
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        throw new NotImplementedException();
+        return new Enumerator(this);
     }
 
-    public readonly struct Enumerator
+    /// <inheritdoc/>
+    public struct Enumerator : IEnumerator<T>
     {
+        private SkipList<T> _skipList;
+
+        private T? _currentValue;
+        
+        private SkipListNode? _currentNode;
+
         /// <inheritdoc/>
         public T Current { get; }
+
+        object IEnumerator.Current => Current;
+
+        public Enumerator(SkipList<T> skipList)
+        {
+            _skipList = skipList;
+            _currentValue = default;
+            _currentNode = skipList._head;
+        }
 
         /// <inheritdoc/>
         public void Dispose()
         {
-            
         }
         
         /// <inheritdoc/>
@@ -107,14 +260,18 @@ public class SkipList<T> : IList<T>
         {
             return true;
         }
+
+        /// <inheritdoc/>
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    private class SkipListElement
+    private class SkipListNode(T value, int levelsAmount)
     {
-        public SkipListElement? Next { get; set; }
-        
-        public int Level { get; set; }
-        
-        public T? Value { get; set; }
+        public T Value { get; } = value;
+
+        public SkipListNode[] Next { get; set; } = new SkipListNode[levelsAmount];
     }
 }
--- SkipList/SkipListTests/SkipListTests.cs
+++ SkipList/SkipListTests/SkipListTests.cs
@@ -1,20 +1,39 @@
-
+using SkipListLibrary;
 
 namespace SkipListTests;
 
 public class Tests
 {
+    private SkipList<int> _skipList = [];
+
+    [TearDown]
+    public void CleanUp()
+    {
+        _skipList = [];
+    }
+
     [Test]
     public void InsertValueToList_ShouldThrowNotSupportedException()
     {
-        
+        Assert.Throws<NotSupportedException>(() => _skipList.Insert(0, 0));
     }
-    
+
     [Test]
     public void SetValueByIndex_ShouldThrowNotSupportedException()
     {
-        
+        _skipList.Add(0);
+        Assert.Throws<NotSupportedException>(() => _skipList[0] = 1);
     }
+
+    [TestCase(int.MaxValue)]
+    [TestCase(-1)]
+    public void GetValue_WithInvalidIndex_ShouldThrowArgumentOutOfRangeException(int index)
+    {
+        _skipList.Add(0);
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = _skipList[index]);
+    }
+
+    
 }
 
 public class TestDataClass
